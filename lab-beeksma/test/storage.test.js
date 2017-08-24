19ace8c33597bd6ce1af6ba556c3d363944851f1
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-24 11:26:13 -0500

    test storage

---
+++ lab-beeksma/test/storage.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const {expect} = require('chai');
+const storage = require('../model/storage');
+
+describe('storage', function(){
+  const schemaName = 'people';
+  const itemToSave = {id:12,name:'keith'};
+  describe('create item', function(){
+    it('should save item',function(done){
+      storage.createItem(schemaName,itemToSave)
+        .then(saveItem => {
+          expect(saveItem).to.deep.equal(itemToSave);
+          done();
+        });
+    });
+  });
+  describe('fetchItem',function(){
+    it('should fetch item', function(done){
+      storage.fetchItem(schemaName, itemToSave.id)
+        .then(fetchedItem =>{
+          expect(fetchedItem).to.deep.equal(itemToSave);
+          done();
+        });
+    });
+  });
+});
